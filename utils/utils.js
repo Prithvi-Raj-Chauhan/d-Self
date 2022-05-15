70916a263fc8b16da123190580b28681d187b1f5
Prithvi Raj Chauhan <divyadollypadhy@outlook.com>
2022-05-15 17:05:32 +0530

    organized utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,17 +1,28 @@
 const { createHash } = require('crypto')
+const today = new Date();
 
 function hash(input){
     return createHash('md5').update(input).digest('hex');
 }
 
-function todaysHash(){
-    const today = new Date();
+function getDate(){
     const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    return date;
+}
+
+function getTime(){
     const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const complete = date+' '+time
+    return time;
+}
+
+function completeDateTime(){
+    return getDate()+' '+getTime();
+}
+
+function todaysHash(){
     return {
-        hash: hash(complete),
-        dateTime: date
+        hash: hash(completeDateTime()),
+        dateTime: getDate()
     }
 }
 module.exports = todaysHash()
